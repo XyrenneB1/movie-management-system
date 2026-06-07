5e04db64996c4c048b61ad52fbcc5a1be5d12e41
XyrenneB1 <xyrennethessamae.bastian@edu.sait.ca>
2026-06-06 19:03:58 -0600

    Inserted Movie.java attributes

--- movie.java
+++ movie.java
@@ -1,1 +1,19 @@
-testing
+/**
+ * Assignment 1: Classess and Objects
+ * Authors: Xyrenne Bastian, Mary Ann Coloma
+ * Date: June 7, 2026
+ */
+
+puublic class Movie {
+    int duration;
+    Strig title;
+    int year;
+
+    public Movie(int duration, String title, int year) {
+        this.duration = duration;
+        this.title = title;
+        this.year = year;
+
+        
+    }
+}
