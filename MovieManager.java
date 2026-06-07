bfe2e723b71efdf564c7e5518904fe9abb7b7d2f
XyrenneB1 <xyrennethessamae.bastian@edu.sait.ca>
2026-06-06 19:44:13 -0600

    Added displayMenu method

--- MovieManager.java
+++ MovieManager.java
@@ -14,9 +14,34 @@ public class MovieManager {
     }
 
     public void displayMenu() {
-        System.out.println("Movie Management system");
-        System.out.println("1 Add New Movie and Save");
-        System.out.println("2 Generate List of Movies Released in a Year");
-        System.out.println("3 Generate List of Random Movies");
-        System.out.println("4 Exit");
+        int option = 0;
+
+        while (option != 4) {
+            System.out.println("Movie Management system");
+            System.out.println("1 Add New Movie and Save");
+            System.out.println("2 Generate List of Movies Released in a Year");
+            System.out.println("3 Generate List of Random Movies");
+            System.out.println("4 Exit");
+            System.out.println();
+            System.out.println("Enter an option: ");
+
+            option = input.nextInt();
+            input.nextLine();
+            System.out.println();
+
+            if (option == 1) {
+                addMovie();
+            } else if (option == 2) {
+                generateMoviesListInYear();
+            }else if (option == 3) {
+                generateRandomMovieList();
+            }else if (option == 4) {
+                saveMovieListToFile();
+            }else {
+                System.out.println("Invalid option!");
+                System.out.println();
+            }
+            
+        }
+        
     }
